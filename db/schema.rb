d3d0b17876106dbe6ad37da8d9f083fe17f40cfa
shleen <sim.sheline@gmail.com>
2018-11-08 15:29:28 +0800

    Add admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_060022) do
+ActiveRecord::Schema.define(version: 2018_11_08_072835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_060022) do
     t.datetime "updated_at", null: false
     t.string "type"
     t.bigint "stall_id"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["stall_id"], name: "index_users_on_stall_id"
