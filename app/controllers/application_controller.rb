2b5c4bf8f0af07a2737f11c410368745c4fe749f
shleen <sim.sheline@gmail.com>
2018-11-09 16:03:18 +0800

    nothing beats abstraction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :set_up_instance_variables
 
   def index
-    if current_user.type == 'Staff'
-      @orders = Order.all.order(created_at: :asc)
-      @order_items = []
-      @orders.each do |o|
-        items = []
-        o.order_items.each do |oi|
-          if oi.menu_item.stall_id == current_user.stall_id
-            items.push(oi)
-          end
-        end
-        @order_items.push(items) if !items.empty?
-      end
-    end
-    puts @order_items.to_s
+    get_order_items if current_user.type == 'Staff'
   end
 
   private
@@ -24,5 +11,19 @@ class ApplicationController < ActionController::Base
   def set_up_instance_variables
     # Raspberry PI URL
     @RPI = "https://httpbin.org/post"
+  end
+
+  def get_order_items
+    @orders = Order.all.order(created_at: :asc)
+    @order_items = []
+    @orders.each do |o|
+      items = []
+      o.order_items.each do |oi|
+        if oi.menu_item.stall_id == current_user.stall_id
+          items.push(oi)
+        end
+      end
+      @order_items.push(items) if !items.empty?
+    end
   end
 end
